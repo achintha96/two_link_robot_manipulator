90e8b6c955485be726dacf7aa07aaf74efa1efe4
achintha96 <achinthasurathrathnayake@gmail.com>
2023-02-16 15:21:56 +0530

    FK and arm plotting added. fault in IK

--- Assignment_180534N.m
+++ Assignment_180534N.m
@@ -2,8 +2,8 @@ close all
 clear all
 clc
 
-L1 = 10;
-L2 = 10;
+L_1 = 10;
+L_2 = 10;
 
 start_pt = [20 0 0];
 path = [ [17 1 0]; [10 15 0]; [3 1 0]; [5 5 0]; [15 5 0] ];
@@ -22,10 +22,22 @@ for i=1:no_pts
         x = current_pt(1,1);
         y = current_pt(2,1);
         
-        joint_angles = find_IK(x,y,L1,L2);
-        axis([-0.1 20 -0.1 20])
-        plot(x,y,'d','MarkerFaceColor','red','MarkerEdgeColor','red')
-        pause(0.001)
+        [theta_1,theta_2] = find_IK(x,y,L_1,L_2);
+        %theta_1 = joint_angles(1,1)
+        %theta_2 = joint_angles(1,2)
+        
+        [X1,Y1,X2,Y2] = find_FK(theta_1,theta_2,L_1,L_2);
+        
+        axis([-10 20 -10 20])
+        link1 = line([0 X1], [0 Y1]);
+        link2 = line([X1 X2], [Y1 Y2]);
+        if i>1
+            plot(X2,Y2,'d','MarkerFaceColor','red','MarkerEdgeColor','red')
+            
+        end
+        pause(0.0001)
+        delete(link1)
+        delete(link2)
     end
     %at the end
     start_pt = path(i,:);
