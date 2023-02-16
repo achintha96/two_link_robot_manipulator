909e0122722611a0da8b9527eb5cece36af1e368
achintha96 <achinthasurathrathnayake@gmail.com>
2023-02-16 12:42:46 +0530

    inverse kinematics function completed, not tested

--- Assignment_180534N.m
+++ Assignment_180534N.m
@@ -1,26 +1,28 @@
 close all
 clear all
 clc
+
 a = [1 14 0];
 b = [10 5 0];
 
 [p,v] = generate_trajectory(a,b);
 
-figure
-subplot (3 ,2 ,1)
-plot(p(1,:))
+% figure
+% subplot (3 ,2 ,1)
+% plot(p(1,:))
+% 
+% subplot (3 ,2 ,3)
+% plot(p(2,:))
+% 
+% subplot (3 ,2 ,5)
+% plot(p(3,:))
+% 
+% subplot (3 ,2 ,2)
+% plot(v(1,:))
+% 
+% subplot (3 ,2 ,4)
+% plot(v(2,:))
+% 
+% subplot (3 ,2 ,6)
+% plot(v(3,:))
 
-subplot (3 ,2 ,3)
-plot(p(2,:))
-
-subplot (3 ,2 ,5)
-plot(p(3,:))
-
-subplot (3 ,2 ,2)
-plot(v(1,:))
-
-subplot (3 ,2 ,4)
-plot(v(2,:))
-
-subplot (3 ,2 ,6)
-plot(v(3,:))
---
+++ find_IK.m
@@ -0,0 +1,9 @@
+function [JointAngles] = find_IK(xPos,yPos,L1,L2)
+
+theta2 = acos((xPos^2 + yPos^2 - L1^2 - L2^2)/(2*L1*L2));
+theta1 = atan2(yPos,xPos) - atan2((L2*sin(theta2)),(L1 + L2*cos(theta2)));
+
+JointAngles = [theta1 theta2]; 
+
+end
+
